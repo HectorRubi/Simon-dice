90e6ae518dbfd0f344c3026426bcdf0fdde39ffc
Hector Rubi <hector.rubi.garcia@outlook.com>
2020-08-16 23:53:06 -0500

    Obtener el input del usuario

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@ class Juego {
   }
 
   inicializar() {
+    this.elegirColor = this.elegirColor.bind(this)
     btnEmpezar.classList.add('hide');
     this.nivel = 1
     this.colores = {
@@ -29,6 +30,7 @@ class Juego {
 
   siguienteNivel() {
     this.iluminarSecuencia()
+    this.agregarEventosClick()
   }
 
   transformarNumeroAColor(numero) {
@@ -58,6 +60,17 @@ class Juego {
 
   apagarColor(color) {
     this.colores[color].classList.remove('light')
+  }
+
+  agregarEventosClick() {
+    this.colores.celeste.addEventListener('click', this.elegirColor)
+    this.colores.verde.addEventListener('click', this.elegirColor)
+    this.colores.violeta.addEventListener('click', this.elegirColor)
+    this.colores.naranja.addEventListener('click', this.elegirColor)
+  }
+
+  elegirColor(ev) {
+    console.log(this)
   }
 }
 
